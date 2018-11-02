13eb9dbc875f166e8f33cf1a9d644f91813c72fe
Robert Leahy <rleahy@rleahy.ca>
2018-11-02 13:24:43 +0000

    Remove Spurious Doxygen Comment
    Doxygen comment in cpp file.

--- src/core/system_error.cpp
+++ src/core/system_error.cpp
@@ -5,17 +5,6 @@
 
 namespace mcpp {
 
-/**
- *  Obtains a `std::error_code` which encapsulates
- *  the same error (or lack thereof) as a
- *  `boost::system::error_code`.
- *
- *  \param [in] ec
- *    The `boost::system::error_code`.
- *
- *  \return
- *    A `std::error_code` equivalent to `ec`.
- */
 std::error_code to_error_code(boost::system::error_code ec) noexcept {
   return std::error_code(ec.value(),
                          ec.category());
